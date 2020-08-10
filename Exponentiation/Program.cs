6e9a6059bcd55883d6f233c83608df722e269d72
Suresh-Thota <Suresh.Thota@innroad.com>
2020-08-10 21:24:53 +0530

    Add project files.

---
+++ Exponentiation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exponentiation
+{
+    class Program
+    {
+        public static double FindExponentiation(double x, int n)
+        {
+            if (n == 0)
+                return 1;
+            return x * FindExponentiation(x, n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            double d;
+            Console.Write(" Enter exponential value ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("enter base value: ");
+            d = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("exponential value is : " + FindExponentiation(d, n));
+        }
+    }
+}
